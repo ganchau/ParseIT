78c08aec4d73e273ecad254cb1e76dd0ac040a52
ganchau <gancchau@gmail.com>
2015-08-15 09:38:56 -0400

    initial commit

---
+++ ParseIT/ParseIT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ParseIT
+//
+//  Created by Gan Chau on 8/15/15.
+//  Copyright (c) 2015 GC App. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ParseIT/ParseIT/Constants.h
@@ -0,0 +1,13 @@
+//
+//  Constants.h
+//  ParseIT
+//
+//  Created by Gan Chau on 8/15/15.
+//  Copyright (c) 2015 GC App. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Constants : NSObject
+
+@end
